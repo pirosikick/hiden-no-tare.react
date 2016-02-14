f90737eb7ebd582ecd681742fa4ff0eddb486da4
Hiroyuki Anai <pirosikick@gmail.com>
2016-02-14 23:59:47 +0900

    Updated depedencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 import Container from './Container';
 import One from './One';
 import Two from './Two';
 
 const App = props => (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Container}>
       <Route path="one" component={One}/>
       <Route path="two" component={Two}/>
