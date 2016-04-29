ae1a3d582c28421ebc20ba0e653bfff91cb5d502
Hiroyuki Anai <pirosikick@gmail.com>
2016-04-30 08:42:37 +0900

    Updated react to 15.0.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import Container from './Container';
 import One from './One';
 import Two from './Two';
 
 const App = props => (
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={Container}>
       <Route path="one" component={One}/>
       <Route path="two" component={Two}/>
